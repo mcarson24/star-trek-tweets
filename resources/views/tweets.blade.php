fde2736ab6e502d7201449fff198139fc81277c2
Matthew Carson <matthew.carson@pm.me>
2021-03-10 10:11:01 -0500

    Set overflow on tweet body to scroll

--- resources/views/tweets.blade.php
+++ resources/views/tweets.blade.php
@@ -6,7 +6,7 @@
 			<p class="w-1/4 hidden lg:block">{{ $tweet->fullTime }}</p>
 			<p class="w-1/4 lg:hidden">{{ $tweet->shortTime }}</p>
 			<div class="w-3/4 px-2">
-				<p>{!! $tweet->formattedText !!}</p>
+				<p class="overflow-scroll">{!! $tweet->formattedText !!}</p>
 			</div>
 		</div>
 	@endforeach
